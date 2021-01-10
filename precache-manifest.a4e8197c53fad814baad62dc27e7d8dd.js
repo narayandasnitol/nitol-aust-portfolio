8af972a53255012e6272fe11fafb1adc1a84dc1a
Narayan Das Nitol <nitol.aust.cse@gmail.com>
2021-01-11 01:23:42 +0600

    Updates

---
+++ precache-manifest.a4e8197c53fad814baad62dc27e7d8dd.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba58f1773f587b7c9785d035581c658",
+    "url": "/nitol-aust-portfolio/index.html"
+  },
+  {
+    "revision": "a00b2a5c6793911f6949",
+    "url": "/nitol-aust-portfolio/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "b84ae2f3a679e4f4d7e3",
+    "url": "/nitol-aust-portfolio/static/css/4.ee2a0142.chunk.css"
+  },
+  {
+    "revision": "d2d41d3d7069e746be5a",
+    "url": "/nitol-aust-portfolio/static/css/main.8d4182d2.chunk.css"
+  },
+  {
+    "revision": "7d022dce45d56f10410d",
+    "url": "/nitol-aust-portfolio/static/js/2.e749d436.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/nitol-aust-portfolio/static/js/2.e749d436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a00b2a5c6793911f6949",
+    "url": "/nitol-aust-portfolio/static/js/3.1e39b1a0.chunk.js"
+  },
+  {
+    "revision": "b84ae2f3a679e4f4d7e3",
+    "url": "/nitol-aust-portfolio/static/js/4.a3a6f920.chunk.js"
+  },
+  {
+    "revision": "d2d41d3d7069e746be5a",
+    "url": "/nitol-aust-portfolio/static/js/main.5423ab8e.chunk.js"
+  },
+  {
+    "revision": "a56ae838571e972c0c2a",
+    "url": "/nitol-aust-portfolio/static/js/runtime-main.3f28d99b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/nitol-aust-portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4738f523c741a6d9a7949ac220b514f0",
+    "url": "/nitol-aust-portfolio/static/media/AustBus.4738f523.png"
+  },
+  {
+    "revision": "0959eae69300291c1e7152b76b0dd579",
+    "url": "/nitol-aust-portfolio/static/media/AustCar.0959eae6.png"
+  },
+  {
+    "revision": "aa421ee276b8cc22c55512e5d074ea50",
+    "url": "/nitol-aust-portfolio/static/media/AustCgpa.aa421ee2.png"
+  },
+  {
+    "revision": "045b9163010e4d82fd84c2df72fcfac4",
+    "url": "/nitol-aust-portfolio/static/media/AustCgpa1.045b9163.png"
+  },
+  {
+    "revision": "221453d37b431c4bb82d4aa8f521da8a",
+    "url": "/nitol-aust-portfolio/static/media/AustClassManager.221453d3.png"
+  },
+  {
+    "revision": "1e3b533259681173337605f6052304dd",
+    "url": "/nitol-aust-portfolio/static/media/AustClassManager1.1e3b5332.png"
+  },
+  {
+    "revision": "5d360ee5e254e8dcc8445be7da712def",
+    "url": "/nitol-aust-portfolio/static/media/Duck.5d360ee5.png"
+  },
+  {
+    "revision": "947ab267a535767ebf59e197b7a23895",
+    "url": "/nitol-aust-portfolio/static/media/LogoAust.947ab267.jpg"
+  },
+  {
+    "revision": "0f00ff18540f0d8112a7ed6931505a77",
+    "url": "/nitol-aust-portfolio/static/media/LogoBgpsc.0f00ff18.jpg"
+  },
+  {
+    "revision": "7d6ea242fb182b2fe88c8d7c2d48823d",
+    "url": "/nitol-aust-portfolio/static/media/LogoJcpsc.7d6ea242.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/nitol-aust-portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/nitol-aust-portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/nitol-aust-portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/nitol-aust-portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/nitol-aust-portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/nitol-aust-portfolio/static/media/talksCardBack.39eff4a9.svg"
+  }
+]);
